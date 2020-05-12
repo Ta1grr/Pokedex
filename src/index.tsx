fa95004b625809e7b93d3101ddcbbe213f239dc1
Ta1grr <taile602@gmail.com>
2020-05-12 14:27:35 -0500

    added in redux-think

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import combineReducers from "./reducers";
+import thunk from "redux-thunk";
 
-const store = createStore(combineReducers);
+const store = createStore(combineReducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
