8dd88899293423ba0892cb5c7a54c49b8297d0db
Ta1grr <taile602@gmail.com>
2020-05-18 00:16:44 -0500

    cleaning up unneeded comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-// import logo from "./logo.svg";
 import "./App.css";
-// import MainContent from "./components/main-content/content/index";
 import PokedexList from "./components/main-content/pokedexlist/index";
-
-interface IAppProps {
-  //stuff
-}
 
 export const App: React.FC<{}> = () => {
   return (
